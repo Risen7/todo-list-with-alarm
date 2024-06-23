8b7dbe9760a930efdfe734e1c307489623f4f85f
Risen7 <144148376+Risen7@users.noreply.github.com>
2024-06-23 20:44:40 +0800

    Add files via upload

---
+++ scriptBackUp01.js
@@ -0,0 +1,207 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("change", filterTodo);
+
+let dateIn = new Date();
+let dateStr = dateIn.toDateString(); 
+
+
+function addTodo(event) {
+    let todoInputDate = `${todoInput.value} - ${dateStr}`;
+    let todoInputDat = `${todoInput.value}<span class"encTime">${dateStr}</span>`
+    event.preventDefault();
+    const timeInput = document.createElement("span")
+    timeInput.classList.add("encTime");
+    timeInput.innerText = dateStr;
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todoal");
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value; 
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    newTodo.appendChild(timeInput);
+
+    //ADDING TO LOCAL STORAGE 
+    // saveLocalTodos(todoInput.value,);
+    saveLocalTodos(todoInputDat)
+
+    
+    const btnDiv = document.createElement("div");
+    btnDiv.classList.add("btn-div");
+    todoDiv.appendChild(btnDiv);
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></li>';
+    completedButton.classList.add("complete-btn");
+    btnDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-x"></li>';
+    trashButton.classList.add("trash-btn");
+    btnDiv.appendChild(trashButton);
+    
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+    console.log(todoInputDate);
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    // console.log(item);
+
+    if(item.classList[0] === "trash-btn") {
+        const todoal = item.parentElement.parentElement;
+        todoal.classList.add("slide");
+
+        removeLocalTodos(todoal);
+        todoal.addEventListener("transitionend", function() {
+            todoal.remove();
+        });
+    }
+
+    if(item.classList[0] === "complete-btn") {
+        const todoal = item.parentElement.parentElement;
+        todoal.classList.toggle("completed");
+    }
+
+    console.log(item.parentElement);    
+}
+
+function filterTodo(e) {
+    const todosal = todoList.childNodes;
+    todosal.forEach(function(todoal) {
+        switch(e.target.value) {
+            case "all": 
+                todoal.style.display = "flex";
+                break;
+            case "completed": 
+                if(todoal.classList.contains("completed")) {
+                    todoal.style.display = "flex";
+                } else {
+                    todoal.style.display = "none";
+                }
+                break;
+            case "incomplete":
+                if(!todoal.classList.contains("completed")) {
+                    todoal.style.display = "flex";
+                } else {
+                    todoal.style.display = "none";
+                }
+                break;
+        }
+    });
+}
+
+function saveLocalTodos(todoal) {
+    let todosal;
+    if(localStorage.getItem("todosal") === null) {
+        todosal = [];
+    } else {
+        todosal = JSON.parse(localStorage.getItem("todosal"));
+    }
+    todosal.push(todoal);
+    localStorage.setItem("todosal", JSON.stringify(todosal));
+}
+
+function getLocalTodos() {
+    let todosal;
+    if(localStorage.getItem("todosal") === null) {
+        todosal = [];
+    } else {
+        todosal = JSON.parse(localStorage.getItem("todosal"));
+    }
+    todosal.forEach(function(todoal) {
+        // const todoDiv = document.createElement("div");
+        // todoDiv.classList.add("todo");
+        // const newTodo = document.createElement("li");
+        // newTodo.innerText = todo;
+        // newTodo.classList.add("todo-item");
+        // todoDiv.appendChild(newTodo);
+
+        let todoInputDate = `${todoInput.value} - ${dateStr}`;
+        // event.preventDefault();
+        const timeInput = document.createElement("span")
+        // timeInput.classList.add("encTime");
+        // timeInput.innerText = dateStr;
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todoal");
+        const newTodo = document.createElement("li");
+        newTodo.innerHTML = todoal; 
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+        // newTodo.appendChild(timeInput);
+
+        const btnDiv = document.createElement("div");
+        btnDiv.classList.add("btn-div");
+        todoDiv.appendChild(btnDiv);
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = '<i class="fas fa-check"></li>';
+        completedButton.classList.add("complete-btn");
+        btnDiv.appendChild(completedButton);
+
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = '<i class="fas fa-x"></li >';
+        trashButton.classList.add("trash-btn");
+        btnDiv.appendChild(trashButton);
+
+        todoList.appendChild(todoDiv);
+
+        console.log(localStorage.getItem("todosal"))
+    });
+}
+
+function removeLocalTodos(todoal) {
+    let todosal;
+    if(localStorage.getItem("todosal") === null) {
+        todosal = [];
+    } else {
+        todosal = JSON.parse(localStorage.getItem("todosal"));
+    }
+
+    const todoIndex = todoal.children[0].innerText;
+    todosal.splice(todosal.indexOf(todoIndex), 1);
+    localStorage.setItem("todosal", JSON.stringify(todosal));
+}
+
+
+// TIME CLOCK --------------------------------------------------------------------------->
+
+const myLabel = document.getElementById("myLabel");
+
+
+update();
+setInterval(update, 1000);
+
+function update(){
+
+    let date = new Date();
+    myLabel.innerHTML = `${date.toDateString()} - ${formatTime(date)}`;
+
+    function formatTime(){
+        let hours = date.getHours();
+        let mins = date.getMinutes();
+        let secs = date.getSeconds();
+
+        let amOrPm = hours >= 12 ? "PM" : "AM";  //to get AM or PM on standards hours
+
+        hours = (hours % 12) || 12;  //to make military time 24hour to 12hours standard
+        
+        hours = formatZeroes(hours);
+        mins = formatZeroes(mins);
+        secs = formatZeroes(secs);
+
+        return`${hours}:${mins}:${secs} ${amOrPm}`
+    }
+
+    function formatZeroes(time){
+        time = time.toString();
+        return time.length < 2 ? "0" + time : time;
+    }
+
+
+}
---
+++ scriptBakUp.js
@@ -0,0 +1,207 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+document.addEventListener("DOMContentLoaded", getLocalTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("change", filterTodo);
+
+let dateIn = new Date();
+let dateStr = dateIn.toDateString(); 
+
+
+function addTodo(event) {
+    let todoInputDate = `${todoInput.value} - ${dateStr}`;
+    let todoInputDat = `${todoInput.value}<span class"encTime">${dateStr}</span>`
+    event.preventDefault();
+    const timeInput = document.createElement("span")
+    timeInput.classList.add("encTime");
+    timeInput.innerText = dateStr;
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    const newTodo = document.createElement("li");
+    newTodo.innerText = todoInput.value; 
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    newTodo.appendChild(timeInput);
+
+    //ADDING TO LOCAL STORAGE 
+    // saveLocalTodos(todoInput.value,);
+    saveLocalTodos(todoInputDat)
+
+    
+    const btnDiv = document.createElement("div");
+    btnDiv.classList.add("btn-div");
+    todoDiv.appendChild(btnDiv);
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></li>';
+    completedButton.classList.add("complete-btn");
+    btnDiv.appendChild(completedButton);
+
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-x"></li>';
+    trashButton.classList.add("trash-btn");
+    btnDiv.appendChild(trashButton);
+    
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+    console.log(todoInputDate);
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    // console.log(item);
+
+    if(item.classList[0] === "trash-btn") {
+        const todo = item.parentElement.parentElement;
+        todo.classList.add("slide");
+
+        removeLocalTodos(todo);
+        todo.addEventListener("transitionend", function() {
+            todo.remove();
+        });
+    }
+
+    if(item.classList[0] === "complete-btn") {
+        const todo = item.parentElement.parentElement;
+        todo.classList.toggle("completed");
+    }
+
+    console.log(item.parentElement);    
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch(e.target.value) {
+            case "all": 
+                todo.style.display = "flex";
+                break;
+            case "completed": 
+                if(todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "incomplete":
+                if(!todo.classList.contains("completed")) {
+                    todo.style.display = "flex";
+                } else {
+                    todo.style.display = "none";
+                }
+                break;
+        }
+    });
+}
+
+function saveLocalTodos(todo) {
+    let todos;
+    if(localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.push(todo);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function getLocalTodos() {
+    let todos;
+    if(localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+    todos.forEach(function(todo) {
+        // const todoDiv = document.createElement("div");
+        // todoDiv.classList.add("todo");
+        // const newTodo = document.createElement("li");
+        // newTodo.innerText = todo;
+        // newTodo.classList.add("todo-item");
+        // todoDiv.appendChild(newTodo);
+
+        let todoInputDate = `${todoInput.value} - ${dateStr}`;
+        // event.preventDefault();
+        const timeInput = document.createElement("span")
+        // timeInput.classList.add("encTime");
+        // timeInput.innerText = dateStr;
+        const todoDiv = document.createElement("div");
+        todoDiv.classList.add("todo");
+        const newTodo = document.createElement("li");
+        newTodo.innerHTML = todo; 
+        newTodo.classList.add("todo-item");
+        todoDiv.appendChild(newTodo);
+        // newTodo.appendChild(timeInput);
+
+        const btnDiv = document.createElement("div");
+        btnDiv.classList.add("btn-div");
+        todoDiv.appendChild(btnDiv);
+        const completedButton = document.createElement("button");
+        completedButton.innerHTML = '<i class="fas fa-check"></li>';
+        completedButton.classList.add("complete-btn");
+        btnDiv.appendChild(completedButton);
+
+        const trashButton = document.createElement("button");
+        trashButton.innerHTML = '<i class="fas fa-x"></li >';
+        trashButton.classList.add("trash-btn");
+        btnDiv.appendChild(trashButton);
+
+        todoList.appendChild(todoDiv);
+
+        console.log(localStorage.getItem("todos"))
+    });
+}
+
+function removeLocalTodos(todo) {
+    let todos;
+    if(localStorage.getItem("todos") === null) {
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    const todoIndex = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoIndex), 1);
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+// TIME CLOCK --------------------------------------------------------------------------->
+
+const myLabel = document.getElementById("myLabel");
+
+
+update();
+setInterval(update, 1000);
+
+function update(){
+
+    let date = new Date();
+    myLabel.innerHTML = `${date.toDateString()} - ${formatTime(date)}`;
+
+    function formatTime(){
+        let hours = date.getHours();
+        let mins = date.getMinutes();
+        let secs = date.getSeconds();
+
+        let amOrPm = hours >= 12 ? "PM" : "AM";  //to get AM or PM on standards hours
+
+        hours = (hours % 12) || 12;  //to make military time 24hour to 12hours standard
+        
+        hours = formatZeroes(hours);
+        mins = formatZeroes(mins);
+        secs = formatZeroes(secs);
+
+        return`${hours}:${mins}:${secs} ${amOrPm}`
+    }
+
+    function formatZeroes(time){
+        time = time.toString();
+        return time.length < 2 ? "0" + time : time;
+    }
+
+
+}
